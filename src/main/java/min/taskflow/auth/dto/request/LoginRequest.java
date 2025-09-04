133942bdce13eb9861838924d755d3f73b13e10b
kimseokjun <k03s13j@naver.com>
2025-09-04 10:44:34 +0900

    refactor/auth: login 리뷰ëdto 인자값 줄바꿈

--- src/main/java/min/taskflow/auth/dto/request/LoginRequest.java
+++ src/main/java/min/taskflow/auth/dto/request/LoginRequest.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 /*
 로그인시 요청dto
  */
-public record LoginRequest(@NotBlank(message = "유저네임은 공백일 수 없습니다.") String username,
-                           @NotBlank(message = "비밀번호는 공백일 수 없습니다.") String password) {
+public record LoginRequest(
+        @NotBlank(message = "유저네임은 공백일 수 없습니다.") String username,
+        @NotBlank(message = "비밀번호는 공백일 수 없습니다.") String password) {
 }
--- src/main/java/min/taskflow/auth/dto/response/RegisterResponse.java
+++ src/main/java/min/taskflow/auth/dto/response/RegisterResponse.java
@@ -9,11 +9,12 @@ import java.time.LocalDateTime;
 회원가입시 응답 DTO
  */
 @Builder
-public record RegisterResponse(Long id,
-                               String username,
-                               String email,
-                               String name,
-                               UserRole role,
-                               LocalDateTime createdAt) {
+public record RegisterResponse(
+        Long id,
+        String username,
+        String email,
+        String name,
+        UserRole role,
+        LocalDateTime createdAt) {
 
 }
