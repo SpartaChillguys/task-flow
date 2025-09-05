8cd048032316b847c6a4c42ebc6676545e931d35
Main <twodc@naver.com>
2025-09-05 20:26:27 +0900

    feat/comment: 댓글 조회 API 구현 및 테스트 코드 작성 (Git 충돌 문제로 다시 올립니다.)

--- src/main/java/min/taskflow/comment/dto/response/CommentResponse.java
+++ src/main/java/min/taskflow/comment/dto/response/CommentResponse.java
@@ -1,11 +1,13 @@
 package min.taskflow.comment.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import min.taskflow.user.dto.response.UserResponse;
 
 import java.time.LocalDateTime;
 
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CommentResponse(
 
         Long commentId,
@@ -16,4 +18,5 @@ public record CommentResponse(
         Long parentId,
         LocalDateTime createdAt,
         LocalDateTime updatedAt
-) {}
+) {
+}
