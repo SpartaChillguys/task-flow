53f0aa6f5a10850ff137f5de8ffb8faf949ef5c2
kimseokjun <k03s13j@naver.com>
2025-09-07 23:02:00 +0900

    feat/log: 로그 조회 파람추가버전

--- src/main/java/min/taskflow/log/Service/ActivityLogService.java
+++ src/main/java/min/taskflow/log/Service/ActivityLogService.java
@@ -12,8 +12,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -35,15 +37,53 @@ public class ActivityLogService {
         logRepository.save(log);
     }
 
-
-    public Page<ActivityLogResponse> getLogs(int page, int size) {
+    public Page<ActivityLogResponse> getLogs(
+            int page,
+            int size,
+            ActivityType type,
+            Long userId,
+            Long taskId,
+            LocalDate startDate,
+            LocalDate endDate
+    ) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "timeStamp"));
 
-        return logRepository.findAll(pageable)
-                .map(log -> {
-                    // 지금은 userId 따로 없으니 userName 기준으로 조회
-                    UserProfileResponse userProfile = internalQueryUserService.findByName(log.getUserName());
-                    return logMapper.toActivityLogResponse(log, userProfile);
-                });
+        Specification<Log> spec = (root, query, cb) -> cb.conjunction();
+
+        if (type != null) {
+            spec = spec.and((root, query, cb) -> cb.equal(root.get("type"), type));
+        }
+        if (userId != null) {
+            spec = spec.and((root, query, cb) -> cb.equal(root.get("userId"), userId));
+        }
+        if (taskId != null) {
+            spec = spec.and((root, query, cb) -> cb.equal(root.get("taskId"), taskId));
+        }
+        if (startDate != null && endDate != null) {
+            spec = spec.and((root, query, cb) -> cb.between(
+                    root.get("timeStamp"),
+                    startDate.atStartOfDay(),
+                    endDate.atTime(23, 59, 59)
+            ));
+        }
+
+        Page<Log> logs = logRepository.findAll(spec, pageable);
+
+        return logs.map(log -> {
+            UserProfileResponse userProfile = internalQueryUserService.findByName(log.getUserName());
+            return logMapper.toActivityLogResponse(log, userProfile);
+        });
     }
+
+
+//    public Page<ActivityLogResponse> getLogs(int page, int size) {
+//        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "timeStamp"));
+//
+//        return logRepository.findAll(pageable)
+//                .map(log -> {
+//                    // 지금은 userId 따로 없으니 userName 기준으로 조회
+//                    UserProfileResponse userProfile = internalQueryUserService.findByName(log.getUserName());
+//                    return logMapper.toActivityLogResponse(log, userProfile);
+//                });
+//    }
 }
--- src/main/java/min/taskflow/log/controller/ActivityLogController.java
+++ src/main/java/min/taskflow/log/controller/ActivityLogController.java
@@ -2,14 +2,18 @@ package min.taskflow.log.controller;
 
 import lombok.RequiredArgsConstructor;
 import min.taskflow.common.response.ApiResponse;
+import min.taskflow.log.ActivityType;
 import min.taskflow.log.Service.ActivityLogService;
 import min.taskflow.log.response.ActivityLogResponse;
 import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,14 +22,32 @@ public class ActivityLogController {
 
     private final ActivityLogService activityLogService;
 
+//    @GetMapping("/activities")
+//    public ResponseEntity<ApiResponse<Page<ActivityLogResponse>>> getLogs(
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "10") int size
+//    ) {
+//        Page<ActivityLogResponse> data = activityLogService.getLogs(page, size);
+//
+//        return
+//                ApiResponse.success(data, "활동 로그를 조회했습니다.");
+//    }
+
     @GetMapping("/activities")
     public ResponseEntity<ApiResponse<Page<ActivityLogResponse>>> getLogs(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) ActivityType type,
+            @RequestParam(required = false) Long userId,
+            @RequestParam(required = false) Long taskId,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate
     ) {
-        Page<ActivityLogResponse> data = activityLogService.getLogs(page, size);
+        Page<ActivityLogResponse> data =
+                activityLogService.getLogs(page, size, type, userId, taskId, startDate, endDate);
 
         return
                 ApiResponse.success(data, "활동 로그를 조회했습니다.");
+
     }
 }
